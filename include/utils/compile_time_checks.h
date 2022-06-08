2eb04b769e7d63b0d758c898ecc6f3a8c9475bdf
Tim Herreijgers <timtim3001@gmail.com>
2022-06-09 00:01:22 +0200

    Added documentation

--- include/utils/compile_time_checks.h
+++ include/utils/compile_time_checks.h
@@ -3,13 +3,27 @@
 
 namespace Utils
 {
+/**
+ * \internal
+ * Namespace containing all compile time checks. These are used to create better (and more advanced) error
+ * messages using static_assert
+ */
 namespace CompileTimeChecks
 {
+
+	/**
+	 * \internal
+	 * Checks whether two types are the same. This is the is not equal variant
+	 */
     template<class T, class U>
     struct is_same {
         enum { value = 0 };
     };
 
+	/**
+	 * \internal
+	 * Checks whether two types are the same. This is the is equal variant
+	 */
     template<class T>
     struct is_same<T, T> {
         enum { value = 1 };
