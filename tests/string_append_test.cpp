21d0e3d359c1d356d2f936661e32878c9a0d2634
Tim Herreijgers <timtim3001@gmail.com>
2022-06-16 00:17:31 +0200

    Fixed naming, added array initialization

--- tests/string_append_test.cpp
+++ tests/string_append_test.cpp
@@ -4,34 +4,34 @@
 
 TEST(StringAppendTest, StringAppendUnsafe_OneInputString_GeneratesOutputString)
 {
-    char buffer[32];
+    char buffer[32] = {0};
     const auto result = Utils::appendStrings(buffer, 32, "test");
 
     ASSERT_TRUE(strcmp(buffer, "test"));
     ASSERT_EQ(result, true);
 }
 
-TEST(StringAppendUnsafeTest, StringAppend_TwoInputString_GeneratesOutputString)
+TEST(StringAppendTest, StringAppend_TwoInputString_GeneratesOutputString)
 {
-    char buffer[32];
+    char buffer[32] = {0};
     const auto result = Utils::appendStrings(buffer, 32, "test", "Tset");
 
     ASSERT_TRUE(strcmp(buffer, "testTset"));
     ASSERT_EQ(result, true);
 }
 
-TEST(StringAppendUnsafeTest, StringAppend_ThreeInputString_GeneratesOutputString)
+TEST(StringAppendTest, StringAppend_ThreeInputString_GeneratesOutputString)
 {
-    char buffer[32];
+    char buffer[32] = {0};
     const auto result = Utils::appendStrings(buffer, 32, "test", "Tset", "bla");
 
     ASSERT_TRUE(strcmp(buffer, "testTsetbla"));
     ASSERT_EQ(result, true);
 }
 
-TEST(StringAppendUnsafeTest, StringAppend_MultipleInputStringOverflowingBuffer_GeneratesPartialOutputStringAndReturnsFalse)
+TEST(StringAppendTest, StringAppend_MultipleInputStringOverflowingBuffer_GeneratesPartialOutputStringAndReturnsFalse)
 {
-    char buffer[32];
+    char buffer[32] = {0};
     const auto result = Utils::appendStrings(buffer, 32, "This string is long",
                                              ". This string makes it longer then 32 bytes, which should not be possible to add");
 
--- tests/string_append_unsafe_test.cpp
+++ tests/string_append_unsafe_test.cpp
@@ -4,7 +4,7 @@
 
 TEST(StringAppendUnsafeTest, StringAppendUnsafe_OneInputString_GeneratesOutputString)
 {
-    char buffer[32];
+    char buffer[32] = {0};
     Utils::appendStringsUnsafe(buffer, "test");
 
     ASSERT_TRUE(strcmp(buffer, "test"));
@@ -12,7 +12,7 @@ TEST(StringAppendUnsafeTest, StringAppendUnsafe_OneInputString_GeneratesOutputSt
 
 TEST(StringAppendUnsafeTest, StringAppendUnsafe_TwoInputString_GeneratesOutputString)
 {
-    char buffer[32];
+    char buffer[32] = {0};
     Utils::appendStringsUnsafe(buffer, "test", "Tset");
 
     ASSERT_TRUE(strcmp(buffer, "testTset"));
@@ -20,7 +20,7 @@ TEST(StringAppendUnsafeTest, StringAppendUnsafe_TwoInputString_GeneratesOutputSt
 
 TEST(StringAppendUnsafeTest, StringAppendUnsafe_ThreeInputString_GeneratesOutputString)
 {
-    char buffer[32];
+    char buffer[32] = {0};
     Utils::appendStringsUnsafe(buffer, "test", "Tset", "bla");
 
     ASSERT_TRUE(strcmp(buffer, "testTsetbla"));
