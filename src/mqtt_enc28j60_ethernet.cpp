239c968b050aa5cb65ffd2cd13def099ce0c71fd
Tim Herreijgers <timtim3001@gmail.com>
2022-07-28 00:12:35 +0200

    [#10] add clang format (#20)
    * Added .clang-format file
    
    * Added format me script
    
    * Edited clang-format
    
    * Ran clang-format
    
    * Removed old clang-format configuration

--- src/mqtt_enc28j60_ethernet.cpp
+++ src/mqtt_enc28j60_ethernet.cpp
@@ -1,37 +1,42 @@
 #include "mqtt_enc28j60_ethernet.h"
+#include "domotics_debug.h"
+
 #include <EthernetENC.h>
 #include <stdlib.h>
 
-#include "domotics_debug.h"
 
-MqttEnc28J60Ethernet::MqttEnc28J60Ethernet(const char *name, uint8_t mac[6], int slaveSelectPin): Mqtt(name, _ethClient)
+MqttEnc28J60Ethernet::MqttEnc28J60Ethernet(const char * name, uint8_t mac[6], int slaveSelectPin) :
+    Mqtt(name, _ethClient)
 {
-	memcpy(_mac, mac, 6);
-	Ethernet.init(slaveSelectPin);
+    memcpy(_mac, mac, 6);
+    Ethernet.init(slaveSelectPin);
 }
 
-bool MqttEnc28J60Ethernet::connect(const IPAddress &brokerIp)
+bool MqttEnc28J60Ethernet::connect(const IPAddress & brokerIp)
 {
-	Ethernet.begin(_mac);
+    Ethernet.begin(_mac);
 
-	if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-  	  DEBUG_PRINTLN("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-  	  	while (true) ;
-  	}
+    if (Ethernet.hardwareStatus() == EthernetNoHardware)
+    {
+        DEBUG_PRINTLN("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+        while (true)
+            ;
+    }
 
-	delay(100);
+    delay(100);
 
-  	if (Ethernet.linkStatus() == LinkOFF) {
-  	  	Serial.println(F("Ethernet cable is not connected."));
-  	}
+    if (Ethernet.linkStatus() == LinkOFF)
+    {
+        Serial.println(F("Ethernet cable is not connected."));
+    }
 
-	DEBUG_PRINTLN("Connected to Ethernet");
-	DEBUG_PRINT("LocalIP: ");
-	DEBUG_PRINTLN(Ethernet.localIP());
+    DEBUG_PRINTLN("Connected to Ethernet");
+    DEBUG_PRINT("LocalIP: ");
+    DEBUG_PRINTLN(Ethernet.localIP());
 
-	DEBUG_PRINTLN("Connecting to MQTT broker");
-	bool res = connectToBroker(brokerIp);
-	DEBUG_PRINTLN("Connected to broker");
+    DEBUG_PRINTLN("Connecting to MQTT broker");
+    bool res = connectToBroker(brokerIp);
+    DEBUG_PRINTLN("Connected to broker");
 
-	return res;
+    return res;
 }
