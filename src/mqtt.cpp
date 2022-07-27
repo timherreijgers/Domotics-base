f303b8a3341d1d4741d232294175ab2abda56678
Tim Herreijgers <timtim3001@gmail.com>
2022-07-27 23:46:19 +0200

    Ran clang-format

--- src/mqtt.cpp
+++ src/mqtt.cpp
@@ -1,67 +1,67 @@
 #include "mqtt.h"
 #include "domotics_debug.h"
 
-static void onMessageReceived(char* topic, uint8_t* payload, unsigned int length);
+static void onMessageReceived(char * topic, uint8_t * payload, unsigned int length);
 
-Mqtt::Mqtt(const char *name, Client &client): m_name(name)
+Mqtt::Mqtt(const char * name, Client & client) :
+    m_name(name)
 {
-	m_mqttClient = PubSubClient(client);
-	m_mqttClient.setBufferSize(512);
+    m_mqttClient = PubSubClient(client);
+    m_mqttClient.setBufferSize(512);
 }
 
 Mqtt::~Mqtt()
 {
-	if(m_mqttClient.connected())
-		m_mqttClient.disconnect();
+    if (m_mqttClient.connected())
+        m_mqttClient.disconnect();
 }
 
-bool Mqtt::publishOnTopic(const char *topic, const char *payload)
+bool Mqtt::publishOnTopic(const char * topic, const char * payload)
 {
-	if(!m_mqttClient.connected())
-		return false;
+    if (!m_mqttClient.connected())
+        return false;
 
-	return m_mqttClient.publish(topic, payload);
-}	
+    return m_mqttClient.publish(topic, payload);
+}
 
-bool Mqtt::connectToBroker(const IPAddress &brokerIp)
+bool Mqtt::connectToBroker(const IPAddress & brokerIp)
 {
     m_mqttClient.setServer(brokerIp, 1883);
-	m_mqttClient.setCallback(onMessageReceived);
-	m_mqttClient.setKeepAlive(5);
+    m_mqttClient.setCallback(onMessageReceived);
+    m_mqttClient.setKeepAlive(5);
 
-	while(!m_mqttClient.connect(m_name)) 
-	{
-		DEBUG_PRINT(".");
-		delay(1000);
-	}
-	DEBUG_PRINTLN("");
+    while (!m_mqttClient.connect(m_name))
+    {
+        DEBUG_PRINT(".");
+        delay(1000);
+    }
+    DEBUG_PRINTLN("");
 
-	return true;
+    return true;
 }
 
-bool Mqtt::subscribeToTopic(const char *topic)
+bool Mqtt::subscribeToTopic(const char * topic)
 {
-	return m_mqttClient.subscribe(topic);
+    return m_mqttClient.subscribe(topic);
 }
 
 bool Mqtt::loop()
 {
-	if(!m_mqttClient.connected())
-	{
-		DEBUG_PRINT("Not connected, error code: ");
-		DEBUG_PRINTLN(m_mqttClient.state());
-		m_mqttClient.connect(m_name);
-	}
+    if (!m_mqttClient.connected())
+    {
+        DEBUG_PRINT("Not connected, error code: ");
+        DEBUG_PRINTLN(m_mqttClient.state());
+        m_mqttClient.connect(m_name);
+    }
 
-	return m_mqttClient.loop();
+    return m_mqttClient.loop();
 }
 
-static void onMessageReceived(char* topic, uint8_t* payload, unsigned int length)
+static void onMessageReceived(char * topic, uint8_t * payload, unsigned int length)
 {
-	mqttMessageReceived(topic, payload, length);
+    mqttMessageReceived(topic, payload, length);
 }
 
-__attribute__((weak)) void mqttMessageReceived(const char *topic, const uint8_t *payload, uint32_t length)
+__attribute__((weak)) void mqttMessageReceived(const char * topic, const uint8_t * payload, uint32_t length)
 {
-
 }
