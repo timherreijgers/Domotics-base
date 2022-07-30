fa9088f0647c85a7fb9594268dd1e32723fc0ad5
Tim Herreijgers <timtim3001@gmail.com>
2022-07-30 10:14:13 +0200

    Added empty unit test and class

--- tests/mqtt_message_parser_test.cpp
+++ tests/mqtt_message_parser_test.cpp
@@ -2,42 +2,13 @@
 
 #include <gtest/gtest.h>
 
-template <typename FirstType, typename SecondType>
-struct TestStruct
-{
-    FirstType value1;
-    SecondType value2;
-};
-
 class MqttMessageParserTest : public ::testing::Test
 {
 
 };
 
-TEST_F(MqttMessageParserTest, messageConsistingOfNoDataThrowsError)
+TEST_F(MqttMessageParserTest, test)
 {
-    EXPECT_ANY_THROW(MqttMessageParser parser = MqttMessageParser<int>::Factory().build());
-}
-
-TEST_F(MqttMessageParserTest, messageConsistingOfOneIntParsesReturnsInt)
-{
-    MqttMessageParser parser = MqttMessageParser<int>::Factory().parseAsInt().build();
-    int result = 0;
-
-    parser.parse("100", result);
-
-    ASSERT_EQ(result, 100);
-}
-
-TEST_F(MqttMessageParserTest, messageConsistingOfTwoIntsWithCommaDelimiterStructContainingCorrectInts)
-{
-    using DataType = TestStruct<int, int>;
-
-    MqttMessageParser parser = MqttMessageParser<DataType>::Factory().delimiter(',').parseAsInt().parseAsInt().build();
-    DataType data;
-
-    parser.parse("100,200", data);
-
-    ASSERT_EQ(data.value1, 100);
-    ASSERT_EQ(data.value2, 200);
+    MqttMessageParser parser();
+    ASSERT_TRUE(true);
 }
