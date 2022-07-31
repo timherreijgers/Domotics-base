c6dc5da6776eedf7d9f36e75221e3ae05fc9e3a7
Tim Herreijgers <timtim3001@gmail.com>
2022-07-31 16:46:19 +0200

    Added parsing of ints

--- tests/mqtt_message_parser_test.cpp
+++ tests/mqtt_message_parser_test.cpp
@@ -2,13 +2,37 @@
 
 #include <gtest/gtest.h>
 
+struct TestInt
+{
+    int value1;
+    int value2;
+    int value3;
+};
+
 class MqttMessageParserTest : public ::testing::Test
 {
 
 };
 
-TEST_F(MqttMessageParserTest, test)
+TEST_F(MqttMessageParserTest, messageConsistingOfNoDataThrowsError)
 {
-    MqttMessageParser parser();
-    ASSERT_TRUE(true);
+    EXPECT_ANY_THROW(MqttMessageParser parser = MqttMessageParser<int>::Factory().build());
 }
+
+TEST_F(MqttMessageParserTest, messageConsistingOfOneIntParsesReturnsInt)
+{
+    MqttMessageParser parser = MqttMessageParser<int>::Factory().parseAsInt().build();
+    int result = 0;
+
+    parser.parse("100", result);
+
+    ASSERT_EQ(result, 100);
+}
+
+//TEST_F(MqttMessageParserTest, messageConsistingOfTwoIntsWithCommaDelimiterStructContainingCorrectInts)
+//{
+//    MqttMessageParser parser = MqttMessageParser<int>::Factory().parseAsInt().build();
+//    const auto result = parser.parse("100");
+//
+//    ASSERT_EQ(result, 100);
+//}
