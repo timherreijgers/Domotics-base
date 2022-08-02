5959447e6d58f0be69d4a3f6ea90960486720378
Tim Herreijgers <timtim3001@gmail.com>
2022-08-03 00:22:06 +0200

    Added documentation for the MqttMessageParser class

---
+++ include/mqtt/parsing_functions.h
@@ -0,0 +1,31 @@
+#ifndef __PARSING_FUNCTIONS_H__
+#define __PARSING_FUNCTIONS_H__
+
+/**
+ * Namespace containing predefined parsing functions for the \ref MqttMessageParser
+ */
+namespace ParsingFunctions {
+
+template <typename T>
+int addToBuffer(const T& value, char * buffer);
+
+int parseInt(const char * str, char * buffer);
+int parseFloat(const char * str, char * buffer);
+
+template<typename T, T(* ParsingFunction)(const char * str)>
+int parseCustom(const char * str, char * buffer)
+{
+    const auto value = ParsingFunction(str);
+    return ParsingFunctions::addToBuffer(value, buffer);
+}
+
+template <typename T>
+int addToBuffer(const T& value, char * buffer)
+{
+    memcpy(buffer, &value, sizeof(value));
+    return sizeof(value);
+}
+
+} // namespace ParsingFunctions
+
+#endif // __PARSING_FUNCTIONS_H__
