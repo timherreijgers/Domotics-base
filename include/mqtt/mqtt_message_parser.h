f38010bfd9997c500d40605ffc1223982fbbe6f1
Tim Herreijgers <timtim3001@gmail.com>
2022-08-02 22:49:51 +0200

    Generalized the parsing of enums even more

--- include/mqtt/mqtt_message_parser.h
+++ include/mqtt/mqtt_message_parser.h
@@ -11,8 +11,18 @@ using MessageParserParseFunction = int(*)(const char * str, char * buffer);
 
 namespace ParsingFunctions {
 
+template <typename T>
+int addToBuffer(const T& value, char * buffer);
+
 int parseInt(const char * str, char * buffer);
 int parseFloat(const char * str, char * buffer);
+
+template<typename T, T(* ParsingFunction)(const char * str)>
+int parseCustom(const char * str, char * buffer)
+{
+    const auto value = ParsingFunction(str);
+    return ParsingFunctions::addToBuffer(value, buffer);
+}
 
 template <typename T>
 int addToBuffer(const T& value, char * buffer)
