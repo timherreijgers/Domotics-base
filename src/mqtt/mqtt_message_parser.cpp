e1c54e0581f902a843fd81cd2cacd256ce51df2b
Tim Herreijgers <timtim3001@gmail.com>
2022-08-10 20:43:58 +0200

    Changed from strtof to atof

--- src/mqtt/mqtt_message_parser.cpp
+++ src/mqtt/mqtt_message_parser.cpp
@@ -10,7 +10,7 @@ int parseInt(const char * str, char * buffer)
 
 int parseFloat(const char * str, char * buffer)
 {
-    const auto value = strtof(str, nullptr);
+    const auto value = (float) atof(str);
     return addToBuffer(value, buffer);
 }
 
